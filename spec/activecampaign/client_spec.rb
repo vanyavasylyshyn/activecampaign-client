d99816092e6bfce87d13518c791d59e81c2996dd
Vanya <vanyavasylyshyn@gmail.com>
2022-01-19 11:40:27 +0200

    Technical/Gem structure initialization

---
+++ spec/activecampaign/client_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Activecampaign::Client do
+  it "has a version number" do
+    expect(Activecampaign::Client::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
